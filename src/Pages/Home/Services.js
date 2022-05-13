d8865a392e5bf6c170433626bb7fc36a771e8f3c
najmul009 <najmul.tk16@gmail.com>
2022-05-13 17:46:09 +0600

    service banner added

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -3,6 +3,7 @@ import fluoride from '../../assets/images/fluoride.png'
 import cavity from '../../assets/images/cavity.png'
 import whitening from '../../assets/images/whitening.png'
 import ServiceCard from './ServiceCard';
+import Servicebanner from './Servicebanner';
 
 const Services = () => {
     const services =[
@@ -32,13 +33,16 @@ const Services = () => {
                 <h2 className='text-4xl'>Services We Provied</h2>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
                 {
                     services.map(service=> <ServiceCard
                     key={service._id}
                     service={service}
                     ></ServiceCard>)
                 }
+            </div>
+            <div>
+                <Servicebanner></Servicebanner>
             </div>
         </div>
     );
